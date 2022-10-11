c4c45ff0c53c74d5e2dd8b5173207720f2d4a389
ArtemPavl <artem.pavlenko.93@inbox.ru>
2022-10-11 21:53:10 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Домашнее задание к уроку 9");
+        // Задание 1
+        // System.out.println("Задание 1");
+        // int[] numbers = new int[]{ 1, 2, 3};
+        // double[] fractions = {1.57, 7.654, 9.986};
+        // int[] speeds = {60 , 90, 110};
+
+        // Задание 2
+        // System.out.println("Задание 2");
+
+    }
+}
